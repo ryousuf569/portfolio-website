b031286b275cdd286abd27e5046eb3395f0feb04
Yousuf Rashid <yousufrashidcoding@gmail.com>
2026-01-06 21:58:55 -0500

    updated resume

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -48,7 +48,7 @@ export function Experience() {
             </div>
             
             <a
-              href="https://drive.google.com/file/d/1TaGVbuR-O4PZVfVOymT98kVvYE_Oiuxj/view?usp=sharing"
+              href="https://drive.google.com/file/d/1sQKYUPf28fSVjuyZ9fWnzR4HL5fcIjzE/view?usp=sharing"
               target="_blank"
               rel="noopener noreferrer"
               className="inline-flex items-center gap-2 px-6 py-3 bg-[#1ed760] text-black rounded-full hover:bg-[#1fdf64] transition-all hover:scale-105"
